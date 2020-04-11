4ffb75d7e8190a6eb537c3607309716642af24b4
alextanhongpin <alextan220990@gmail.com>
2020-04-11 18:39:41 +0800

    chore: udpate parser logic

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ png:
 	make compile-png
 
 compile-%:
-	go run main.go | dot -T$*> out.$*
+	# go run main.go | dot -T$*> out.$*
+	go run main.go -in in.txt -out out.$*
 	open out.$*
 
 test:
--- main.go
+++ main.go
@@ -1,63 +1,303 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
 	"io/ioutil"
 	"log"
+	"os"
+	"os/exec"
 	"regexp"
 	"strings"
-
-	"github.com/alextanhongpin/go-db-parser/parser"
-	"github.com/alextanhongpin/go-db-parser/renderer"
 )
 
+type Template struct {
+	Title     string
+	Relations []Relation
+	Entities  []Entity
+}
+
+type Attribute struct {
+	Name string
+}
+
+func NewAttribute(name string) Attribute {
+	return Attribute{strings.TrimSpace(name)}
+}
+
+func (a Attribute) primary() bool {
+	return strings.HasPrefix(a.Name, "*") || a.foreignAndPrimary()
+}
+
+func (a Attribute) foreignAndPrimary() bool {
+	return strings.HasPrefix(a.Name, "*+") || strings.HasPrefix(a.Name, "+*")
+}
+
+func (a Attribute) foreign() bool {
+	return strings.HasPrefix(a.Name, "+") || a.foreignAndPrimary()
+}
+
+func (a Attribute) String() string {
+	name := a.Name
+	if a.primary() {
+		name = fmt.Sprintf("<U>%s</U>", name)
+	}
+	if a.foreign() {
+		name = fmt.Sprintf("<I>%s</I>", name)
+	}
+	return name
+}
+
+type Option struct {
+	Color string `json:"color"`
+}
+
+type Entity struct {
+	Comments   []string
+	Name       string
+	Attributes []Attribute
+	Option     Option
+}
+
+type Relation struct {
+	From, To, FromCardinal, ToCardinal string
+}
+
+var relationsMapper = map[string]string{
+	"1": "1",
+	"?": "0..1",
+	"+": "1..N",
+	"*": "0..N",
+}
+
+func (r Relation) String() string {
+	return fmt.Sprintf("%q -> %q[taillabel=%q, headlabel=%q]",
+		r.From,
+		r.To,
+		relationsMapper[r.ToCardinal],
+		relationsMapper[r.FromCardinal],
+	)
+}
+
+func NewRelationFromSlice(r []string) Relation {
+	if len(r) != 4 {
+		return Relation{}
+	}
+	return Relation{
+		From:         strings.TrimSpace(r[0]),
+		To:           strings.TrimSpace(r[3]),
+		FromCardinal: strings.TrimSpace(r[1]),
+		ToCardinal:   strings.TrimSpace(r[2]),
+	}
+}
+
 func main() {
-	content, err := ioutil.ReadFile("in.txt")
+	var in, out string
+	flag.StringVar(&in, "in", "in.txt", "the input file to read")
+	flag.StringVar(&out, "out", "out.png", "the output file to write")
+	flag.Parse()
+	f, err := os.Open(in)
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	var partitions []string
+	var tmp []string
+
+	addToPartition := func() {
+		partitions = append(partitions, strings.Join(tmp, "\n"))
+		tmp = tmp[:0]
+	}
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if len(line) == 0 && len(tmp) > 0 {
+			addToPartition()
+		}
+		tmp = append(tmp, line)
+	}
+	if len(tmp) > 0 {
+		addToPartition()
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "shouldn't see an error scanning a string")
+	}
+
+	var entities []Entity
+	var relations []Relation
+	var title string
+	for _, part := range partitions {
+		entity := matchEntity(part)
+		if t := matchTitle(part); len(t) > 0 {
+			title = t
+		}
+		comments := matchComments(part)
+		relation := matchRelations(part)
+
+		if len(relation) > 0 {
+			for _, r := range relation {
+				relations = append(relations, NewRelationFromSlice(r))
+			}
+		}
+		if len(entity) > 0 {
+			rawOpts := entity[1]
+			opt := Option{Color: "#eeeeee"}
+			_ = json.Unmarshal([]byte(rawOpts), &opt)
+
+			rows := parseEntity(entity[0])
+			name, rawAttributes := rows[0], rows[1:]
+			var attributes []Attribute
+			for _, raw := range rawAttributes {
+				attributes = append(attributes, NewAttribute(raw))
+			}
+
+			e := Entity{
+				Name:       name,
+				Attributes: attributes,
+				Comments:   comments,
+				Option:     opt,
+			}
+
+			entities = append(entities, e)
+		}
+	}
+	var data Template
+	data.Title = title
+	data.Entities = entities
+	data.Relations = relations
+
+	outFile := strings.Split(out, ".")
+	ext := outFile[1]
+	path, _ := exec.LookPath("dot")
+	cmd := exec.Command(path, fmt.Sprintf("-T%s", ext))
+	stdin, err := cmd.StdinPipe()
 	if err != nil {
 		log.Fatal(err)
 	}
-	raw := strings.TrimSpace(string(content))
-	var data renderer.Data
-	relationsMapper := map[string]string{
-		"1": "1",    // exactly 1
-		"*": "0..N", // 0 or more
-		"+": "1..N", // 1 or more
-		"?": "0..1", // 0 or 1
+	go func() {
+		defer stdin.Close()
+		Render(data, stdin)
+	}()
+	writeCmd, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
 	}
-	{
-		re := regexp.MustCompile(`(?m)^Title:([\w ]+)$`)
-		result := re.FindStringSubmatch(raw)
-		if len(result) > 1 {
-			data.Title = strings.TrimSpace(result[1])
+	mode := int(0644)
+	ioutil.WriteFile(out, writeCmd, os.FileMode(mode))
+}
+
+func matchEntity(s string) []string {
+	re := regexp.MustCompile(`(?m)(\[.+\]([{}:"#\n\s\w]+)?[\s\w\[\]!"#$%&'()*+, -./:;<=>?@[\]^_{|}~]+$)`)
+	matches := re.FindAllStringSubmatch(s, -1)
+	if len(matches) > 0 {
+		var result []string
+		for _, r := range matches {
+			result = append(result, r[1:]...)
 		}
+		return result
 	}
-	{
-		re := regexp.MustCompile(`(?m)(\[.+\]([{}:"#\n\s\w]+)?[\s\w\n*+()]+$)`)
-		result := re.FindAllStringSubmatch(raw, -1)
-		for _, entities := range result {
-			entity := parser.ParseEntity(entities[0])
-			var columns []string
-			for _, col := range entity.Columns {
-				columns = append(columns, col.String())
-			}
-			data.Entities = append(data.Entities, renderer.Entity{Title: entity.Header.Name, Columns: columns, Option: entity.Header.Option})
+	return nil
+}
+
+func matchTitle(s string) string {
+	s = strings.TrimSpace(s)
+	result := regexp.MustCompile(`(?m)^Title:([\w\s]+)$`).FindStringSubmatch(s)
+	if len(result) > 0 {
+		return strings.TrimSpace(result[1])
+	}
+	return ""
+}
+
+func matchComments(s string) []string {
+	re := regexp.MustCompile(`(?m)^#([\w!"#$%&'()*+, -./:;<=>?@[\]^_{|}~]+)$`)
+	matches := re.FindAllStringSubmatch(s, -1)
+	if len(matches) > 0 {
+		var result []string
+		for _, r := range matches {
+			result = append(result, r[1:]...)
 		}
+		return result
 	}
-	{
-		re := regexp.MustCompile(`(?m)^([\w\s]+)([?1+*])--([?1+*])([\w\s]+)$`)
-		result := re.FindAllStringSubmatch(raw, -1)
-		for _, relations := range result {
-			fromEntity, toRelation := strings.TrimSpace(relations[1]), relations[2]
-			fromRelation, toEntity := relations[3], strings.TrimSpace(relations[4])
-			if len(data.Relations) == 0 {
-				data.Relations = make([]renderer.Relation, 0)
-			}
-			data.Relations = append(data.Relations, renderer.Relation{
-				From:         fromEntity,
-				To:           toEntity,
-				FromCardinal: relationsMapper[toRelation],
-				ToCardinal:   relationsMapper[fromRelation],
-			})
+	return nil
+}
+
+func matchRelations(s string) [][]string {
+	re := regexp.MustCompile(`(?m)^([\w ]+)([?1+*])--([?1+*])([\w ]+)$`)
+	matches := re.FindAllStringSubmatch(s, -1)
+	if len(matches) > 0 {
+		var result [][]string
+		for _, r := range matches {
+			result = append(result, r[1:])
 		}
+		return result
 	}
-	renderer.Render(data)
+	return nil
+}
+
+func parseEntity(s string) []string {
+	s = strings.TrimSpace(s)
+	lines := strings.Split(s, "\n")
+	head, body := lines[0], lines[1:]
+	name := regexp.MustCompile(`\[(.+?)\]`).FindStringSubmatch(head)[1]
+
+	var result []string
+	result = append(result, strings.TrimSpace(name))
+	for _, row := range body {
+		result = append(result, strings.TrimSpace(row))
+	}
+	return result
+}
+
+var tpl = `
+digraph G {
+    // Title.
+    pencolor=black
+    fontsize=16
+    labelloc=t
+    label = "{{- .Title -}}"
+    rankdir=LR;
+    graph [pad="0.5", nodesep="1", ranksep="2"];
+    //
+    // Box for entities.
+    //
+    node [shape=none, margin=0]
+    //
+    // Relationship Edges.
+    //
+    edge[arrowhead=none, arrowtail=none, dirType=both, style=dashed,color="#888888"];
+    //
+    // Entities.
+    //
+    {{- range $entity := .Entities}}
+    "{{$entity.Name}}" [label={{noescape "<"}}
+	<table border="0" cellborder="1" cellspacing="0" cellpadding="4">
+	    <tr><td  bgColor="{{$entity.Option.Color}}" align="left">{{noescape $entity.Name}}</td></tr>
+	    {{- range $attr := $entity.Attributes}}
+	    <tr><td align="left">{{noescape $attr.String}}</td></tr>
+	    {{- end}}
+	</table>
+    >]
+    {{- end}}
+    //
+    // Relationships.
+    //
+    {{- range $rel := .Relations}}
+    {{noescape $rel.String}}
+    {{- end}}
+}`
+
+func Render(data Template, writer io.Writer) {
+	var noescape = func(value string) template.HTML {
+		return template.HTML(value)
+	}
+	tmpl := template.Must(template.New("").Funcs(template.FuncMap{
+		"noescape": noescape,
+	}).Parse(tpl))
+	tmpl.Execute(writer, data)
 }
